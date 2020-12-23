c767f1c6ec26b8e51177ef19f2adba65758ff692
kzhang01 <karinazhang1@gmail.com>
2020-12-22 20:02:54 -0500

    Create ViewsAndModifiers

---
+++ Day 6/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Karina Zhang on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
