b54b8363f259864a9633e66fe88ccca740f04392
kzhang01 <karinazhang1@gmail.com>
2020-12-28 23:37:33 -0500

    building layout of BetterRest

---
+++ Day 7/BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Karina Zhang on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
