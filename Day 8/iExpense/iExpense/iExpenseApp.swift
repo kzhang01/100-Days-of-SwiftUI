1a2e7ae68ea09e22f96d3dc262885116a7e6bfae
kzhang01 <karinazhang1@gmail.com>
2020-12-31 17:26:32 -0500

    finished iExpense

---
+++ Day 8/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Karina Zhang on 12/31/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
