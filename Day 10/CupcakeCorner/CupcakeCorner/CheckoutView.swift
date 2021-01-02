2bffc68e1b04a4faad536b4f92df633033b7f077
kzhang01 <karinazhang1@gmail.com>
2021-01-01 23:23:20 -0500

    CupcakeCorner UI finished

---
+++ Day 10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,38 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Karina Zhang on 1/1/21.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+                    Button("Place Order") {
+                        
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
---
+++ Day 10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Karina Zhang on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
