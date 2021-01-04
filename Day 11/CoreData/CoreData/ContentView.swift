0c673fe4b5f4802c967bec74e7ee10201b0cae68
kzhang01 <karinazhang1@gmail.com>
2021-01-04 17:47:11 -0500

    CoreData challenge finished

--- Day 11/CoreData/CoreData/ContentView.swift
+++ Day 11/CoreData/CoreData/ContentView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 import CoreData
 
-struct OneToManyRelationshipsContentView: View {
+struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Country.entity(), sortDescriptors: []) var countries: FetchedResults<Country>
-
+    
     var body: some View {
         VStack {
             List {
@@ -50,105 +50,6 @@ struct OneToManyRelationshipsContentView: View {
                 candy4.origin?.fullName = "Switzerland"
 
                 try? self.moc.save()
-            }
-        }
-
-    }
-}
-
-struct DynamicFilteringContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @State private var lastNameFilter = "A"
-
-    var body: some View {
-        VStack {
-            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
-                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
-            }
-
-            Button("Add Examples") {
-                let taylor = Singer(context: self.moc)
-                taylor.firstName = "Taylor"
-                taylor.lastName = "Swift"
-
-                let ed = Singer(context: self.moc)
-                ed.firstName = "Ed"
-                ed.lastName = "Sheeran"
-
-                let adele = Singer(context: self.moc)
-                adele.firstName = "Adele"
-                adele.lastName = "Adkins"
-
-                try? self.moc.save()
-            }
-
-            Button("Show A") {
-                self.lastNameFilter = "A"
-            }
-
-            Button("Show S") {
-                self.lastNameFilter = "S"
-            }
-        }
-    }
-}
-
-struct ShipContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star Wars'")) var ships: FetchedResults<Ship>
-
-    var body: some View {
-        VStack {
-            List(ships, id: \.self) { ship in
-                Text(ship.name ?? "Unknown name")
-            }
-
-            Button("Add Examples") {
-                let ship1 = Ship(context: self.moc)
-                ship1.name = "Enterprise"
-                ship1.universe = "Star Trek"
-
-                let ship2 = Ship(context: self.moc)
-                ship2.name = "Defiant"
-                ship2.universe = "Star Trek"
-
-                let ship3 = Ship(context: self.moc)
-                ship3.name = "Millennium Falcon"
-                ship3.universe = "Star Wars"
-
-                let ship4 = Ship(context: self.moc)
-                ship4.name = "Executor"
-                ship4.universe = "Star Wars"
-
-                try? self.moc.save()
-            }
-        }
-    }
-}
-
-
-struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-
-    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
-
-    var body: some View {
-        VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
-            }
-
-            Button("Add") {
-                let wizard = Wizard(context: self.moc)
-                wizard.name = "Harry Potter"
-            }
-
-            Button("Save") {
-                do {
-                    try self.moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
             }
         }
     }
--- Day 11/CoreData/CoreData/FilteredList.swift
+++ Day 11/CoreData/CoreData/FilteredList.swift
@@ -8,21 +8,32 @@
 import CoreData
 import SwiftUI
 
+enum Predicate: String {
+    case beginsWith = "BEGINSWITH"
+    case contains = "CONTAINS"
+}
+
 struct FilteredList<T: NSManagedObject, Content: View>: View {
     var fetchRequest: FetchRequest<T>
-    var singers: FetchedResults<T> { fetchRequest.wrappedValue }
+    var results: FetchedResults<T> { fetchRequest.wrappedValue }
 
     let content: (T) -> Content
 
     var body: some View {
-        List(fetchRequest.wrappedValue, id: \.self) { singer in
-            self.content(singer)
+        List(fetchRequest.wrappedValue, id: \.self) { result in
+            self.content(result)
         }
     }
 
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+    init(filterKey: String,
+         filterValue: String,
+         predicate: Predicate,
+         sortDescriptors: [NSSortDescriptor],
+         @ViewBuilder content: @escaping (T) -> Content) {
+        fetchRequest = FetchRequest<T>(entity: T.entity(),
+                                            sortDescriptors: sortDescriptors,
+                                            predicate: NSPredicate(format: "%K \(predicate.rawValue)[c] %@", filterKey, filterValue))
         self.content = content
     }
+    
 }
-
