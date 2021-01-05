076b13c11aea286aba3996792829102238d1c316
kzhang01 <karinazhang1@gmail.com>
2021-01-05 17:54:39 -0500

    InstaFilter basic functionality

---
+++ Day 12/InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Karina Zhang on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
