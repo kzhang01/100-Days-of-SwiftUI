52b24b5ad2df0132cfb7962339ee27e0f4809b3f
kzhang01 <karinazhang1@gmail.com>
2021-01-05 23:53:29 -0500

    BucketList basic functionality

---
+++ Day 12/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Karina Zhang on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
